351f7ce84529d5e237e4a5ab9a4f9f185de887bc
riquet28 <eric.merlin3@gmail.com>
2017-07-12 11:39:20 +0200

    Chart component and label unit ok

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,20 +1,20 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import Chart from '../components/chart';
 
 class WeatherList extends Component {
   renderWeather(cityData) {
     const name = cityData.city.name;
     const temps = cityData.list.map(weather => weather.main.temp);
+    const pressures = cityData.list.map(weather => weather.main.pressure);
+    const humidities = cityData.list.map(weather => weather.main.humiditiy);
 
     return (
       <tr key={name}>
         <td>{name}</td>
-        <td>
-          <Sparklines height={120} width={180} data={temps}>
-            <SparklinesLine color="red" />
-          </Sparklines>
-        </td>
+        <td><Chart data={temps} color="orange" units="K" /></td>
+        <td><Chart data={pressures} color="green" units="hPa" /></td>
+        <td><Chart data={humidities} color="black" units="%" /></td>
       </tr>
     );
   }
@@ -25,9 +25,9 @@ class WeatherList extends Component {
         <thead>
           <tr>
             <th>City</th>
-            <th>Temperature</th>
-            <th>Pression</th>
-            <th>Humidité</th>
+            <th>Temperature (K)</th>
+            <th>Pression (hPa)</th>
+            <th>Humidité (%)</th>
           </tr>
         </thead>
         <tbody>
